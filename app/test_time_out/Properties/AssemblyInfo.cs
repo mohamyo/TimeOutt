3070e59a55c33f6cbadf5fbc0b6220bd95374cff
mohamyo <58634257+mohamyo@users.noreply.github.com>
2020-01-17 16:22:02 +0200

    Add files via upload

---
+++ app/test_time_out/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_time_out")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test_time_out")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9aadad52-bec0-4d5a-be60-57994e588e69")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ app/test_time_out/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App;
+
+namespace test_time_out
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ///TestMethod1:
+        game g = new game("Adam");
+        ///TestMethod2,TestMethod3:
+        Playing p = new Playing("Adam", "Yosef");
+        ///TestMethod4:
+        ShowFrindList s = new ShowFrindList("Adam");
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(g.MyUsername=="Adam");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(p.MyUserName == "Adam");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(p.VsPlayer == "Yosef");
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(s.MyUserName == "Adam");
+        }
+      
+    }
+}
